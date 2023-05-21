5ca7f51424557567e2442bb641e2b40d0e1e5aab
Tomás López Coppari <tomas.coppari@mercadolibre.com>
2023-05-21 20:56:32 -0300

    Update Calendar.py
    AGREGUE CONEXION A BQ

--- Calendar.py
+++ Calendar.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 import datetime
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
 # Título de la aplicación
 st.title("Events Calendar - Data Speaks")
