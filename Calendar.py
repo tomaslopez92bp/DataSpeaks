aa6e66df92f418b2f1c8a2da00c6367ee8def5a6
Tomás López Coppari <tomaslopez92bp@gmail.com>
2023-05-21 21:30:20 -0300

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -4,7 +4,10 @@ import datetime
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
 # Título de la aplicación
 st.title("Events Calendar - Data Speaks")
