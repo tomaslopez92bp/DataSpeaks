274cf5f6d9d4f00270e276fda1d838da9d51460c
Tomás López Coppari <tomaslopez92bp@gmail.com>
2023-05-21 21:53:58 -0300

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -5,7 +5,9 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/Zeke/GitHub/DataSpeaks/GoogleCloudAccess.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
 client = bigquery.Client()
 
 # Título de la aplicación
